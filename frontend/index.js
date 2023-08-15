408abee7ff754379d39ab818a8d83f615ecbc9e6
JP Caparas <jp@jpcaparas.com>
2023-08-15 20:52:15 +1200

    Parameterise productType

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-async function fetchAndRenderData() {
+async function fetchAndRenderData(productType) {
+    if (!productType) {
+        alert("Please enter a valid product type ID.");
+        return;
+    }
+
     try {
         // Promise.all should solve race conditions
         const [productsResponse, productTypesResponse] = await Promise.all([
@@ -12,25 +17,34 @@ async function fetchAndRenderData() {
         const productTypes = productTypesResponse.data;
 
         const productTypeMapping = {};
-        for (let pt of productTypes) {
+        for (const pt of productTypes) {
             productTypeMapping[pt.id] = pt.type;
         }
 
         const tableBody = document.getElementById('product-table-body');
-        for (let product of products) {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${product.id}</td>
-                <td>${product.name}</td>
-                <td>${product.description}</td>
-                <td>${product.price}</td>
-                <td>${productTypeMapping[product.productTypeId] || 'Unknown'}</td>
-            `;
-            tableBody.appendChild(row);
+        // Clear previous rows
+        tableBody.innerHTML = '';
+
+        for (const product of products) {
+            if (product.productTypeId === Number(productType)) {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${product.id}</td>
+                    <td>${product.name}</td>
+                    <td>${product.description}</td>
+                    <td>${product.price}</td>
+                    <td>${productTypeMapping[product.productTypeId] || 'Unknown'}</td>
+                `;
+                tableBody.appendChild(row);
+            }
         }
     } catch (error) {
         console.error('Error fetching and rendering data:', error);
     }
 }
 
-fetchAndRenderData();
+window.handleFetchClick = function() {
+    const productTypeId = document.getElementById('product-type-input').value;
+    fetchAndRenderData(productTypeId);
+};
+
